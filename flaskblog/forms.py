17b7fb9da89f309025e303f00fb1528b78b1d95f
NikosNtafopoulos <niklausblockchaindev@yahoo.com>
2018-11-29 19:34:29 +0200

    Added updateProfile form

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField,PasswordField, SubmitField, BooleanField
 from wtforms.validators import  DataRequired, Length,Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -32,3 +33,26 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+# Update the account form controller
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                       validators = [DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators = [DataRequired(), Email()])
+    submit = SubmitField('Update')
+    #custom username and email validation
+    def validate_username(self, username):
+        #Update only if username is different that the one that exists in the database
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken.Please choose a different one')
+
+    def validate_email(self, email):
+        # Update only if email is different that the one that exists in the database
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken.Please choose a different one')
+
