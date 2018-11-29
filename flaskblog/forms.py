acfab85f7d22dedae2fe6990e653b487e9769ae8
NikosNtafopoulos <niklausblockchaindev@yahoo.com>
2018-11-29 20:11:53 +0200

    image extension validation

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField,FileAllowed
 from flask_login import current_user
 from wtforms import StringField,PasswordField, SubmitField, BooleanField
 from wtforms.validators import  DataRequired, Length,Email, EqualTo, ValidationError
@@ -40,6 +41,8 @@ class UpdateAccountForm(FlaskForm):
                        validators = [DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators = [DataRequired(), Email()])
+    # Allowed image files
+    picture = FileField('Update Profile Picture',validators=[FileAllowed(['jpg','png','jpeg'])])
     submit = SubmitField('Update')
     #custom username and email validation
     def validate_username(self, username):
